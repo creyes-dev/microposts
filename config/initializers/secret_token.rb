03726c5e8f6832f0e2b97058a049523f7b35b7d7
creyes-dev <creyes.dev@gmail.com>
2019-07-29 22:43:10 -0300

    nuevo proyecto de rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microposts::Application.config.secret_token = '561651e2da88956b2433fd431993d9c5ab04385688d83536ff8ffd6728bb88b3337acdffc2b4bdf5d20887fab84c0875b1247dbd768f2db2e4e248ab0ad4e1e1'

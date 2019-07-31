e1082d6761bcfd7afaf523bd70e6990495a228ec
creyes-dev <creyes.dev@gmail.com>
2019-07-30 22:34:10 -0300

    crear controller StaticPages

--- comandos.sh
+++ comandos.sh
@@ -253,3 +253,28 @@ Use `bundle info [gemname]` to see where a bundled gem is installed.
       create  spec
       create  spec/spec_helper.rb
 
+~/ruby/microposts$ rails generate rspec:install
+
+   identical  .rspec
+       exist  spec
+   identical  spec/spec_helper.rb
+
+# Crear el controller StaticPages
+
+creyes-dev@creyes:~/ruby/microposts$ rails generate controller StaticPages home help --no-test-framework
+
+      create  app/controllers/static_pages_controller.rb
+       route  get "static_pages/help"
+       route  get "static_pages/home"
+      invoke  erb
+      create    app/views/static_pages
+      create    app/views/static_pages/home.html.erb
+      create    app/views/static_pages/help.html.erb
+      invoke  helper
+      create    app/helpers/static_pages_helper.rb
+      invoke  assets
+      invoke    coffee
+      create      app/assets/javascripts/static_pages.js.coffee
+      invoke    scss
+      create      app/assets/stylesheets/static_pages.css.scss
+

26de46a03c51fa17f56784f718948154cf2ddc1b
creyes-dev <creyes.dev@gmail.com>
2019-08-03 16:16:28 -0300

    usar el metodo before de Rspec en vez de escribir el mismo visit en cada test

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Microposts::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/', to: 'static_pages#home'
-  match '/root', to: 'static_pages#home'
-  match '/home', to: 'static_pages#home'
   match '/help', to: 'static_pages#help' # válido para help_path y /help, la diferencia no importa
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,83 +3,77 @@ require 'spec_helper'
 describe "Static Pages" do
 
   describe "Home page" do
+    before { visit root_path }
+
     it "deberia tener el contenido 'Sample app'" do
-      visit root_path
       page.should have_content('Sample App')
     end
 
     it "deberia tener el título base" do
-      visit root_path
       page.should have_selector('title',
         :text => "Ruby on Rails Tutorial Sample App")
     end
 
     it "deberia no tener un título de página por defecto" do
-      visit root_path
       page.should_not have_selector('title', :text => '| Home')
     end
 
     it "Debería tener el h1 'Sample App'" do
-      visit root_path
       page.should have_selector('h1', text: 'Sample App')
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
+
     it "deberia tener el contenido 'Help'" do
-      visit help_path
       page.should have_content('Help')
     end
 
     it "debería tener el título 'Help'" do
-      visit help_path
       page.should have_selector('title',
       :text => "Ruby on Rails Tutorial Sample App | Help")
     end
 
     it "debería tener el h1 'Help'" do
-      visit help_path
       page.should have_selector('h1', text: 'Help')
     end
   end
 
   describe "About page" do
+    before { visit about_path }
+
     it "deberia tener el contenido 'About Us" do
-      visit about_path
       page.should have_content('About Us')
     end
 
     it "debería tener el título 'About Us'" do
-      visit about_path
       page.should have_selector('title',
       :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
 
     it "debería tener el h1 'About'" do
-      visit about_path
       page.should have_selector('h1', text: 'About Us')
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
+
     it "deberia tener el contenido 'Contact" do
-      visit contact_path
       page.should have_content('Contact')
     end
 
     it "debería tener el título 'Contact'" do
-      visit contact_path
       page.should have_selector('title',
       :text => "Ruby on Rails Tutorial Sample App | Contact")
     end
 
     it "Debería tener el h1 'Contact'" do
-      visit contact_path
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "Deberia tener el título 'Contact'" do
-      visit contact_path
       page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end

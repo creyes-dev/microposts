c0a154118b0a02ba9c57a921a581b4cc65263f7f
creyes-dev <creyes.dev@gmail.com>
2019-08-03 16:31:58 -0300

    simplificación código rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,79 +2,32 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  subject { page }
+
   describe "Home page" do
     before { visit root_path }
-
-    it "deberia tener el contenido 'Sample app'" do
-      page.should have_content('Sample App')
-    end
-
-    it "deberia tener el título base" do
-      page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App")
-    end
-
-    it "deberia no tener un título de página por defecto" do
-      page.should_not have_selector('title', :text => '| Home')
-    end
-
-    it "Debería tener el h1 'Sample App'" do
-      page.should have_selector('h1', text: 'Sample App')
-    end
+    it { should have_content('Sample App') }
+    it { should have_selector('title', :text => "Ruby on Rails Tutorial Sample App") }
+    it { should_not have_selector('title', :text => '| Home') }
+    it { should have_selector('h1', text: 'Sample App') }
   end
-
   describe "Help page" do
     before { visit help_path }
-
-    it "deberia tener el contenido 'Help'" do
-      page.should have_content('Help')
-    end
-
-    it "debería tener el título 'Help'" do
-      page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | Help")
-    end
-
-    it "debería tener el h1 'Help'" do
-      page.should have_selector('h1', text: 'Help')
-    end
+    it { should have_content('Help') }
+    it { should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help") }
+    it { should have_selector('h1', text: 'Help') }
   end
-
   describe "About page" do
     before { visit about_path }
-
-    it "deberia tener el contenido 'About Us" do
-      page.should have_content('About Us')
-    end
-
-    it "debería tener el título 'About Us'" do
-      page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | About Us")
-    end
-
-    it "debería tener el h1 'About'" do
-      page.should have_selector('h1', text: 'About Us')
-    end
+    it { should have_content('About Us') }
+    it { should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us") }
+    it { should have_selector('h1', text: 'About Us') }
   end
-
   describe "Contact page" do
     before { visit contact_path }
-
-    it "deberia tener el contenido 'Contact" do
-      page.should have_content('Contact')
-    end
-
-    it "debería tener el título 'Contact'" do
-      page.should have_selector('title',
-      :text => "Ruby on Rails Tutorial Sample App | Contact")
-    end
-
-    it "Debería tener el h1 'Contact'" do
-      page.should have_selector('h1', text: 'Contact')
-    end
-
-    it "Deberia tener el título 'Contact'" do
-      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact") }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact") }
   end
 end

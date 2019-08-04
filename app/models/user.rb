c3d670d9d6dc90d487460d9da123fbf6073645bd
creyes-dev <creyes.dev@gmail.com>
2019-08-04 01:13:50 -0300

    mail del usuario debe ser unico y su formato debe ser válido

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
   has_many :microposts
 
   validates :name, presence: true, length: 50
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 end

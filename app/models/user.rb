500d2ba85d2ed4b96d2a416c655b40378747fb45
creyes-dev <creyes.dev@gmail.com>
2019-08-05 22:20:25 -0300

    Atributos password y password_confirmation agregados a la clase User, se crean pruebas unitarias de la clase User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
   has_many :microposts
 
   before_save { |user| user.email = email.downcase } # justo antes de guardar usar solo minúsculas en el mail
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 end

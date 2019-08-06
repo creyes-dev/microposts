17fd47a176ae4ad7ef61cd7166398cba195e61b4
creyes-dev <creyes.dev@gmail.com>
2019-08-06 00:31:51 -0300

    layout muestra texto de debug que indica el controller y el action utilizados para obtener la pantalla actual

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :microposts
 
-  before_save { |user| user.email = email.downcase } # justo antes de guardar usar solo minúsculas en el mail
+  before_save { self.email.downcase! } # justo antes de guardar usar solo minúsculas en el mail
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i

1caf7e607be83ab505866d17251c2a9021845221
creyes-dev <creyes.dev@gmail.com>
2019-08-11 21:07:57 -0300

    tamaño del avatar de 50px

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 
   # Devuelve el Gravatar (http://gravatar.com/) para el usuario que corresponda.
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}.png"
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     image_tag(gravatar_url, alt: 'gravatar image')
   end
 end

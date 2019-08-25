708caefbfab182d5b13903bb7968b0becfcb80c4
creyes-dev <creyes.dev@gmail.com>
2019-08-25 18:25:23 -0300

    microposts se ven en la pagina del perfil del usuario

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -64,37 +64,37 @@ describe "UserPages" do
 
     describe "with invalid information" do
       it "should not create a user" do
-      expect { click_button submit }.not_to change(User, :count)
-    end
-  end
-
-  describe "after submission" do
-      before { click_button submit }
-      it { should have_selector 'title', text: 'Sign up' }
-      it { should have_content('error') }
-    end
-  end
-
-  describe "with valid information" do
-    before do
-      fill_in "Name",         with: "Example User"
-      fill_in "Email",        with: "user@example.com"
-      fill_in "Password",     with: "foobar"
-      fill_in "Confirmation", with: "foobar"
-    end
-
-    it "should not create a user" do
-      expect { click_button submit }.to change(User, :count).by(1)
-    end
-
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by_email('user@example.com') }
-        it { should have_selector 'title', text: user.name }
-        it { should have_selector 'div.alert.alert-success', text: 'Welcome' }
-        it { should have_link 'Sign out' }
+        expect { click_button submit }.not_to change(User, :count)
       end
     end
+
+    describe "after submission" do
+        before { click_button submit }
+        it { should have_selector 'title', text: 'Sign up' }
+        it { should have_content('error') }
+      end
+
+      describe "with valid information" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+
+        it "should not create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by_email('user@example.com') }
+          it { should have_selector 'title', text: user.name }
+          it { should have_selector 'div.alert.alert-success', text: 'Welcome' }
+          it { should have_link 'Sign out' }
+        end
+      end
+  end
 
     describe "edit" do
       let(:user) { FactoryGirl.create(:user) }
@@ -130,6 +130,23 @@ describe "UserPages" do
       describe "with invalid information" do
         before { click_button "Save changes" }
         it { should have_content('error') }
+      end
+    end
+
+    describe "profile page" do
+      let(:user) { FactoryGirl.create(:user) }
+      let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+      let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+      before { visit user_path(user) }
+
+      it { should have_selector('h1', text: user.name) }
+      it { should have_selector('title', text: user.name) }
+
+      describe "microposts" do
+        it { should have_content(m1.content) }
+        it { should have_content(m2.content) }
+        it { should have_content(user.microposts.count) }
       end
     end
   end

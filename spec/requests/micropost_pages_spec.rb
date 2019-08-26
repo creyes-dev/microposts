afe1c2610790e6a4106926dd21bd044dab22564e
creyes-dev <creyes.dev@gmail.com>
2019-08-26 19:30:07 -0300

    eliminar microposts

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -12,7 +12,7 @@ describe "Micropost pages" do
 
     describe "with invalid information" do
 
-      it "should not create a micropost" do 
+      it "should not create a micropost" do
         expect { click_button "Post" }.should_not change(Micropost, :count)
       end
 
@@ -26,6 +26,18 @@ describe "Micropost pages" do
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
       it "should create a micropost" do 
         expect { click_button "Post" }.should change(Micropost, :count).by(1)
+      end
+    end
+  end
+
+  describe "micropost destruction" do 
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do 
+        expect { click_link "delete" }.should change(Micropost, :count).by(-1)
       end
     end
   end
